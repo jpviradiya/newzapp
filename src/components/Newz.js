a8b0fe53c057d5a591560a4ca138962ec0a2ec33
Jeel Viradiya <jeelviradiya188@gmail.com>
2023-06-23 13:09:52 +0530

    complete

--- src/components/Newz.js
+++ src/components/Newz.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types'
 import InfiniteScroll from "react-infinite-scroll-component";
 
 const Newz = (props) => {
-  
+
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
@@ -14,9 +14,8 @@ const Newz = (props) => {
   const [loading, setLoading] = useState(false)
   const [page, setPage] = useState(1)
   const [totalResults, setTotalResults] = useState(0)
-  document.title = `${capitalizeFirstLetter(props.category)} - NewzApp`
-
-
+  
+  
 
   const updateNewz = async () => {
     props.setProgress(10)
@@ -31,14 +30,16 @@ const Newz = (props) => {
     setLoading(false)
     props.setProgress(100)
   }
-
+  
   useEffect(() => {
     updateNewz()
+    document.title = `${capitalizeFirstLetter(props.category)} - NewzApp`
+    //eslint-disable-next-line
   }, [])
 
   const fetchMoreData = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pagesize=${props.pageSize}`;
     setPage(page + 1)
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pagesize=${props.pageSize}`;
     let data = await fetch(url)
     let parsedData = await data.json()
     setArticles(articles.concat(parsedData.articles))
@@ -48,7 +49,7 @@ const Newz = (props) => {
 
   return (
     <>
-      <h2 className='text-center mt-5'>NewzApp - Top {capitalizeFirstLetter(props.category)} Headlines</h2>
+      <h2 className='text-center mt-5' style={{margin:'35px 0px',marginTop:'90px'}}>NewzApp - Top {capitalizeFirstLetter(props.category)} Headlines</h2>
       {loading && <Loading />}
 
       <InfiniteScroll
